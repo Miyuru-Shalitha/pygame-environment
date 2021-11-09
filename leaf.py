a0e33f88c761fbd0d87a93ddfe68aa3fb3b88bcf
Miyuru <miyurushalitha@gmail.com>
2021-11-09 13:30:00 +0530

    Improve leavese falling effect

--- leaf.py
+++ leaf.py
@@ -8,14 +8,26 @@ class Leaf(pygame.sprite.Sprite):
         super().__init__()
         self.image = pygame.image.load("images/leaf.png").convert()
         self.image = pygame.transform.scale(self.image, (20, 20))
+        self.image = pygame.transform.rotate(self.image, random.randint(0, 360))
         self.image.set_colorkey(WHITE)
         self.rect = self.image.get_rect()
         self.rect.x = random.randint(0, SCREEN_SIZE[0])
         self.rect.y = 0
         self.x_change = 0
+        self.temp_x_val = 0
+        self.x_val_reverse = False
 
     def update(self, wind_speed):
         self.rect.y += 5
         self.rect.x += self.x_change
 
-        self.x_change = wind_speed + random.uniform(-1, 1)
+        if self.x_val_reverse:
+            self.temp_x_val += 0.1
+            if self.temp_x_val > 5:
+                self.x_val_reverse = True
+        else:
+            self.temp_x_val -= 0.1
+            if self.temp_x_val < -5:
+                self.x_val_reverse = True
+
+        self.x_change = wind_speed + random.uniform(-1, 1) + self.temp_x_val
